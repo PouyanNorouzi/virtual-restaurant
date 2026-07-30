28e29f42433d00acce8f279ab2bf4f58c05f6f15
PouyanNorouzi <irfn7pouyan@gmail.com>
2026-07-29 20:54:08 -0700

    intial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Welcome to Pouyan's Virtual restaurant experience
---
+++ backend/src/main.py
@@ -0,0 +1,19 @@
+"""Echo server using the asyncio API."""
+
+import asyncio
+
+from websockets.asyncio.server import serve
+
+
+async def echo(websocket):
+    async for message in websocket:
+        await websocket.send(message)
+
+
+async def main():
+    server = await serve(echo, "localhost", 8765)
+    await server.serve_forever()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
